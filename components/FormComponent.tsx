144bbab24f0835c54928b31a9022c431e1aa0081
Jacob Peddicord <jacob.peddicord@hey.com>
2022-03-08 11:37:10 -0500

    image uploading

--- components/FormComponent.tsx
+++ components/FormComponent.tsx
@@ -1,82 +1,42 @@
 export const MetaDataForm = () => {
     return(
-        <>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Name
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" type="text" />
-                </div>
+        <div className="flex flex-col gap-5">
+            <div>
+                <label className="mb-1">
+                    Name
+                </label>
+                <input type="text"/>
             </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" >
-                        Tagline
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-password" placeholder=""/>
-                </div>
+            <div>
+                <label className="mb-1" >
+                    Tagline
+                </label>
+                <input placeholder=""  type="text"/>
             </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Description
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" type="text" />
-                </div>
+            <div>
+                <label className="mb-1">
+                    Description
+                </label>
+                <textarea rows={5}/>
             </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                    Categories
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" type="text" />
-                </div>
+            <div>
+                <label className="mb-1">
+                Categories
+                </label>
+                <input type="text"/>
             </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                    Creators
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" type="text" />
-                </div>
-            </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                    Platforms
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" type="text" />
-                </div>
-            </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-                    Created At
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name" type="text" />
-                </div>
+            <div>
+                <label className="mb-1">
+                Created At
+                </label>
+                <input className="border-2 ml-2" type="date"/>
             </div>
             {/*<div className="flex justify-center">
             <div className="mb-3 w-96">
                 <label className="fblock text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">Upload Image</label>
-                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="file" id="formFile"/>
+                <input type="file" id="formFile"/>
             </div>
                 </div>*/}
-        </>
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useWallet } from '@solana/wallet-adapter-react';
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocalStorage } from '../lib/useLocalStorage';
 import { MetaDataForm } from '../components/FormComponent';
 
@@ -15,14 +15,14 @@ const Home: NextPage = () => {
   const [metadataEndpoint, setMetadataEndpoint] = useLocalStorage('endpoint', 'https://openmetagraph.vercel.app');
   const [name, setName] = useLocalStorage('name', '');
   const [description, setDescription] = useLocalStorage('description', '');
-  const [categories, setCategories] = useLocalStorage('categories', new Array());
+  const [categories, setCategories] = useLocalStorage('categories', new Array<string>());
   const [createdAt, setCreatedAt] = useLocalStorage('createdAt', 0);
   const [tagline, setTagline] = useLocalStorage('tagline', '');
-  
+
   // Media
   const [primaryImageCID, updatePrimaryImageCID] = useLocalStorage('primaryImageCID', '');
-  const [imageCIDs, updateImageCIDs] = useLocalStorage('imageCIDs', new Array());
-  const [videoCIDs, updateVideoCIDs] = useLocalStorage('videoCIDs', new Array());
+  const [imageCIDs, updateImageCIDs] = useLocalStorage('imageCIDs', new Array<string>());
+  const [videoCIDs, updateVideoCIDs] = useLocalStorage('videoCIDs', new Array<string>());
   // TODO: Creators
   // TODO: Platforms
 
@@ -53,138 +53,152 @@ const Home: NextPage = () => {
     setIsLoading(false);
   }
 
-  async function saveToWeb3Storage(web3_file: Filelike) {
+  async function saveToWeb3Storage(web3File: Filelike) {
     const web3Client = new Web3Storage({ token: "asdfasdf", endpoint: new URL('https://web3proxy.fly.dev/api/web3/') });
-    return await web3Client.put([web3_file], { wrapWithDirectory: false })
+    return await web3Client.put([web3File], { wrapWithDirectory: false })
+  }
+
+  function moveArrayValue(arr: string[], value: string, moveUp: boolean) {
+    let index = arr.indexOf(value);
+    let insertIndex = moveUp ? index - 1 : index + 1;
+    let imageCIDsCopy = [...imageCIDs];
+    imageCIDsCopy = imageCIDsCopy.filter(item => item !== value);
+    imageCIDsCopy.splice(insertIndex, 0, value);
+    updateImageCIDs(imageCIDsCopy);
   }
 
   return (
     <>
-    <div>
-      <div className='border-b'>
-        <div className='px-4 py-4 mx-auto max-w-4xl flex flex-flex items-center justify-between'>
-          <h1 className='font-bold text-center text-2xl m-0 p-0'>
-            Strangemood
-          </h1>
-          <div className='flex flex-row items-center'>
-            <p className='pr-2'>
-              Metadata Gateway:
+      <div>
+        <div className='border-b'>
+          <div className='px-4 py-4 mx-auto max-w-2xl flex flex-flex items-center justify-between'>
+            <h1 className='font-bold text-center text-2xl m-0 p-0'>
+              Strangemood
+            </h1>
+          </div>
+        </div>
+        <div className='m-auto h-full px-4 py-4 max-w-2xl items-center text-left'>
+          <div className='my-auto text-xl font-bold'>Upload your listing</div>
+          <div className='flex flex-col'>
+            <p className='py-2'>This is text that gives you info about uploading your listing's data</p>
+          </div>
+
+          {/* INFO BOX */}
+          <div className='my-5 border border-black p-2 rounded bg-green-200'>
+            <p className='font-bold'>Using the CLI</p>
+            <p>
+              This is info text about how to use the strangemood cli and a link to download it.
             </p>
-            <select className='border-2 rounded p-1' value={metadataEndpoint} onChange={(e) => setMetadataEndpoint(e.target.value)}>
-              <option value="https://openmetagraph.vercel.app">https://openmetagraph.vercel.app</option>
-              <option value="http://localhost:8000">http://localhost:8000</option>
-            </select>
+          </div>
+
+          {/* METADATA FORM */}
+          <form className="w-full">
+            <div className='border-2 rounded p-2 flex flex-col gap-3'>
+              <div className='border-b-2 text-xl text-center font-bold'>Metadata</div>
+            
+              <MetaDataForm />
+            </div>
+
+            {/* MEDIA FORM */}
+            <div className='border-2 rounded p-2 flex flex-col gap-3'>
+              <div className='border-b-2 text-xl text-center font-bold'>Media</div>
+              <div>
+                <label className='mb-1'>Cover Image</label>
+                {!primaryImageCID &&<input
+                  type="file"
+                  onChange={async (e: any) => {
+                    const file = e.target.files[0];
+                    if (!file) {
+                      updatePrimaryImageCID(``);
+                      return
+                    }
+                    try {
+                      const cid = await saveToWeb3Storage(file);
+                      updatePrimaryImageCID(cid.toString());
+                    } catch (error) {
+                      console.log('Error uploading file: ', error);
+                    }
+                  }}
+                />}
+                {primaryImageCID && 
+                  <div className='flex flex-row gap-2 items-center'>
+                    <button onClick={() => {updatePrimaryImageCID('')}} className='bg-red-500 text-white px-2 py-1 rounded h-min'>Remove</button>
+                    <img className='w-1/2' src={`https://ipfs.io/ipfs/${primaryImageCID}`}></img>
+                  </div>
+                }
+              </div>
+              
+              <div>
+                <label className='mb-1'>Images</label>
+                <input
+                  type="file"
+                  onChange={async (e: any) => {
+                    const file = e.target.files[0];
+                    if (!file) { return }
+                    try {
+                      const cid = await saveToWeb3Storage(file);
+                      updateImageCIDs(imageCIDs => [...imageCIDs, cid]);
+                    } catch (error) {
+                      console.log('Error uploading file: ', error);
+                    }
+                  }}
+                />
+              </div>
+              <div className='flex flex-row w-full w-full flex-wrap'>
+                {imageCIDs.map((cid, index) => (
+                  <div key={cid} className='flex flex-col items-center rounded w-1/3 overflow-hidden p-1'>
+                    <img className='w-full border-t-2 border-x-2 rounded-tr rounded-tl' src={`https://ipfs.io/ipfs/${cid}`}></img>
+                    <div className='flex flex-row w-full'>
+                      <button type='button' disabled={index == 0} onClick={() => {moveArrayValue(imageCIDs, cid, true)}} className='bg-blue-500 text-white px-2 py-1 h-min rounded-bl border-l-2 border-b-2 disabled:bg-gray-400 disabled:text-gray-300'>←</button>
+                      <button type='button' onClick={() => {updateImageCIDs(imageCIDs => imageCIDs.filter(item => item !== cid))}} className='bg-red-500 text-white px-2 py-1 h-min border-b-2 grow'>Remove</button>
+                      <button type='button' disabled={index == (imageCIDs.length - 1)} onClick={() => {moveArrayValue(imageCIDs, cid, false)}} className='bg-blue-500 text-white px-2 py-1 h-min rounded-br border-r-2 border-b-2 disabled:bg-gray-400 disabled:text-gray-300'>→</button>  
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <div className="mb-3 w-96">
+                <label className='mb-1'>Videos</label>
+                <input
+                  type="file"
+                  onChange={async (e: any) => {
+                    const file = e.target.files[0];
+                    if (!file) { return }
+                    try {
+                      const cid = await saveToWeb3Storage(file);
+                      updatePrimaryImageCID(cid.toString());
+                    } catch (error) {
+                      console.log('Error uploading file: ', error);
+                    }
+                  }}
+                />
+                {videoCIDs}
+              </div>
+            </div>
+
+            <div className='border-2 rounded p-2 flex flex-col gap-3'>
+              <div className='border-b-2 text-xl text-center font-bold'>Releases</div>
+
+            </div>
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3"></div>
+              <div className="md:w-2/3">
+                <button className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <div className='mt-5'>
+            <p className='underline'>Legend</p>
+            <ul className='list-disc list-inside'>
+              <li>Put info about fields here</li>
+              <li>Field Name: Sentence or two describing the field.</li>
+            </ul>
           </div>
         </div>
       </div>
-      <div className='m-auto h-full px-4 py-4 max-w-4xl items-center text-left'>
-        <div className='my-auto text-xl font-bold'>Upload your listing</div>
-        <div className='flex flex-col'>
-          <p className='py-2'>This is text that gives you info about uploading your listing's data</p>
-        </div>
-
-        {/* INFO BOX */}
-        <div className='my-5 border border-black p-2 rounded bg-green-200'>
-          <p className='font-bold'>Using the CLI</p>
-          <p>
-            This is info text about how to use the strangemood cli and a link to download it.
-          </p>
-        </div>
-
-        {/* METADATA FORM */}
-        <form className="w-full max-w-sm">
-        <div className='border-2 rounded p-2 flex flex-col gap-3'>
-        <div className='border-b-2 text-xl text-center font-bold'>Metadata</div>
-
-        <MetaDataForm/>
-        </div>
-
-        {/* MEDIA FORM */}
-        <div className='border-2 rounded p-2 flex flex-col gap-3'>
-          <div className='border-b-2 text-xl text-center font-bold'>Media</div>
-          <div className="mb-3 w-96">
-          <label className="fblock text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">Primary Image</label>
-            <input
-              type="file"
-              onChange={async (e: any) => {
-                const file = e.target.files[0];
-                if (!file) {
-                  updatePrimaryImageCID(``);
-                  return
-                }
-                try {
-                  const cid = await saveToWeb3Storage(file);
-                  updatePrimaryImageCID(cid.toString());
-                } catch (error) {
-                  console.log('Error uploading file: ', error);
-                }
-              }}
-            />
-            { primaryImageCID && <img className='w-1/2' src={`https://ipfs.io/ipfs/${primaryImageCID}`}></img>}
-            </div>
-
-            <div className="mb-3 w-96">
-            <label className="fblock text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">Images</label>
-            <input
-              type="file"
-              onChange={async (e: any) => {
-                const file = e.target.files[0];
-                if (!file) { return }
-                try {
-                  const cid = await saveToWeb3Storage(file);
-                  imageCIDs.push(cid.toString());
-                  console.log(imageCIDs);
-                  updateImageCIDs(imageCIDs);
-                } catch (error) {
-                  console.log('Error uploading file: ', error);
-                }
-              }}
-            />
-            { imageCIDs.toString() }
-            </div>
-
-            <div className="mb-3 w-96">
-            <label className="fblock text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">Videos</label>
-            <input
-              type="file"
-              onChange={async (e: any) => {
-                const file = e.target.files[0];
-                if (!file) { return }
-                try {
-                  const cid = await saveToWeb3Storage(file);
-                  updatePrimaryImageCID(cid.toString());
-                } catch (error) {
-                  console.log('Error uploading file: ', error);
-                }
-              }}
-            />
-            { videoCIDs }
-          </div>
-        </div>
-
-        <div className='border-2 rounded p-2 flex flex-col gap-3'>
-          <div className='border-b-2 text-xl text-center font-bold'>Releases</div>
-
-        </div>
-        <div className="md:flex md:items-center">
-        <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-              Submit
-            </button>
-          </div>
-        </div>
-        </form>
-
-        <div className='mt-5'>
-          <p className='underline'>Legend</p>
-          <ul className='list-disc list-inside'>
-            <li>Put info about fields here</li>
-            <li>Field Name: Sentence or two describing the field.</li>
-          </ul>
-        </div>
-      </div>
-    </div>
     </>
 
   );
